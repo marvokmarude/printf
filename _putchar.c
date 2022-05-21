e8bf2e5345f4935f9c7c65ac7efc7e9dead9c365
marvokmarude <59051230+marvokmarude@users.noreply.github.com>
2022-05-21 17:25:37 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,81 +1,40 @@
 #include "main.h"
 
-
-
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(int c)
-
 {
+	static char buf[1024];
+	static int pos;
 
-		static char buf[1024];
-
-			static int pos;
-
-
-
-				if (c == -1 || pos >= 1024)
-
-						{
-
-									write(1, buf, pos);
-
-											pos = 0;
-
-												}
-
-					if (c != -1)
-
-							{
-
-										buf[pos++] = c;
-
-											}
-
-						return (1);
-
+	if (c == -1 || pos >= 1024)
+	{
+		write(1, buf, pos);
+		pos = 0;
+	}
+	if (c != -1)
+	{
+		buf[pos++] = c;
+	}
+	return (1);
 }
-
-
 
 /**
+ * _puts - prints a string with newline
+ * @str: the string to print
  *
- *  * _puts - prints a string with newline
- *
- *   * @str: the string to print
- *
- *    *
- *
- *     * Return: number of characters printed
- *
- *      */
-
+ * Return: number of characters printed
+ */
 int _puts(char *str)
-
 {
+	char *count = str;
 
-		char *count = str;
-
-
-
-			while (*str)
-
-						_putchar(*str++);
-
-				return (str - count);
-
+	while (*str)
+		_putchar(*str++);
+	return (str - count);
 }
-
-
